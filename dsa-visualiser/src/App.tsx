03d345635902fe67395ed797e6da002a80d61500
Justin Tran <justintn28@gmail.com>
2025-05-12 19:25:58 +1000

    feat: added in homepage route

--- dsa-visualiser/src/App.tsx
+++ dsa-visualiser/src/App.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SortingVisualiser } from './components/SortingVisualiser/SortingVisualiser';
 import { PathfindingVisualiser } from './components/PathfindingVisualiser/PathfindingVisualiser';
+import { HomePage } from './components/HomePage/HomePage';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route
             path="/sorting"
-            element={<SortingVisualiser min={5} max={1000} arraySize={250} />}
+            element={<SortingVisualiser min={5} max={950} arraySize={250} />}
           />
           <Route
             path="/pathfinding"
-            element={<PathfindingVisualiser maxRows={20} maxCols={50}/>}
+            element={<PathfindingVisualiser maxRows={20} maxCols={50} />}
           />
         </Routes>
       </BrowserRouter>
